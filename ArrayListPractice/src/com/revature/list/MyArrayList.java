ffb497fc239a9cbe3858cee3069514bb816b1028
Sophia <msophiagavrila@gmail.com>
2019-12-03 14:31:10 -0500

    ArrayList Practice Complete! - Sophia Gavrila

--- ArrayListPractice/src/com/revature/list/MyArrayList.java
+++ ArrayListPractice/src/com/revature/list/MyArrayList.java
@@ -1,30 +1,61 @@
 package com.revature.list;
 
+import java.util.Arrays;
+
 public class MyArrayList {
 
+	private int[] arr = new int[10];
+	private int size = 0;
+	private int capacity = this.arr.length;
+	
 	public void add(int val) {
-		// TODO Auto-generated method stub
 		
+		if (this.size < this.capacity) {
+			this.arr[this.size] = val;
+			this.size++;
+		} else {
+			int oldCap = this.capacity;
+			this.capacity *=2;
+			int newArr[] = new int[this.capacity];
+			for(int i=0; i<oldCap; i++) {
+				newArr[i] = this.arr[i];
+			}
+			newArr[this.size] = val;
+			this.arr = newArr;
+			this.size++;
+		}
 	}
 
 	public void set(int index, int val) {
-		// TODO Auto-generated method stub
 		
+		if(index <= this.size) {
+			this.arr[index] = val;
+		}	
+			this.arr[index] = val;
 	}
 
 	public void remove(int index) {
-		// TODO Auto-generated method stub
-		
+		int[] newArr = new int[this.capacity];
+		for(int i=0, j=0; i<size; i++, j++) {
+			if(i==index) {
+				i++;
+			}
+			newArr[j] = arr[i];
+		}
+		this.arr = newArr;
+		this.size--;
 	}
 
 	public int get(int index) {
-		// TODO Auto-generated method stub
-		return 0;
+		return arr[index];
 	}
 
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		return null;
+		int[] newArr = new int[this.size];
+		for(int i=0; i<this.size; i++) {
+			newArr[i] = this.arr[i];
+		}
+		return Arrays.toString(newArr);
 	}
 }
